100ecbaf0a3a43dcabde3ea83821ec923bc4917d
Asheley McDonald <asheleymcdonald@gmail.com>
2018-04-05 02:21:55 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -11,36 +11,37 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
-function shout(string){
+def speak_to_grandma
+   shout(string){
  +  return string.toUpperCase()
  +};
  +
- +shout("hello");
- +
- +function logShout(string){
- +  return console.log(string.toUpperCase())
- +}
- +
- +logShout("hello")
- +
- +function whisper(string){
- +  return string.toLowerCase();
- +}
- +
- +whisper("HELLO")
- +
- +function logWhisper(string){
- +  return console.log(string.toLowerCase())
- +}
- +
- +logWhisper("HELLO")
- +
- +function sayHiToGrandma(string){
- +  if (string === string.toLowerCase()) return "I can't hear you!";
- +  else if (string === "I love you, Grandma.") return "I love you, too.";
- +  else if (string === string.toUpperCase()) return "YES INDEED!";
- +}
- +
- +sayHiToGrandma("hello");
- +sayHiToGrandma("HELLO");
- +sayHiToGrandma("I love you, Grandma.");
+ +shout("hello")
+ end
+ # +function logShout(string){
+ # +  return console.log(string.toUpperCase())
+ # +}
+ # +
+ # +logShout("hello")
+ # +
+ # +function whisper(string){
+ # +  return string.toLowerCase();
+ # +}
+ # +
+ # +whisper("HELLO")
+ # +
+ # +function logWhisper(string){
+ # +  return console.log(string.toLowerCase())
+ # +}
+ # +
+ # +logWhisper("HELLO")
+ # +
+ # +function sayHiToGrandma(string){
+ # +  if (string === string.toLowerCase()) return "I can't hear you!";
+ # +  else if (string === "I love you, Grandma.") return "I love you, too.";
+ # +  else if (string === string.toUpperCase()) return "YES INDEED!";
+ # +}
+ # +
+ # +sayHiToGrandma("hello");
+ # +sayHiToGrandma("HELLO");
+ # +sayHiToGrandma("I love you, Grandma.");
