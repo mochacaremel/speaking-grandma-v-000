89d4c34047e573b42341824c334a5422616c7bf3
Asheley McDonald <asheleymcdonald@gmail.com>
2018-04-05 02:23:46 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -11,35 +11,12 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
-def speak_to_grandma
-   shout(string){
-   return string.toUpperCase(
- shout("hello")
- end
- # +function logShout(string){
- # +  return console.log(string.toUpperCase())
- # +}
- # +
- # +logShout("hello")
- # +
- # +function whisper(string){
- # +  return string.toLowerCase();
- # +}
- # +
- # +whisper("HELLO")
- # +
- # +function logWhisper(string){
- # +  return console.log(string.toLowerCase())
- # +}
- # +
- # +logWhisper("HELLO")
- # +
- # +function sayHiToGrandma(string){
- # +  if (string === string.toLowerCase()) return "I can't hear you!";
- # +  else if (string === "I love you, Grandma.") return "I love you, too.";
- # +  else if (string === string.toUpperCase()) return "YES INDEED!";
- # +}
- # +
- # +sayHiToGrandma("hello");
- # +sayHiToGrandma("HELLO");
- # +sayHiToGrandma("I love you, Grandma.");
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
+  end
+end
